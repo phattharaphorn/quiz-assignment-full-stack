ce23ced0b2b0c772f6523d8b1c383d05eb309267
phattharaphorn <64010659@kmitl.ac.th>
2024-01-21 04:18:20 +0700

    Update App.vue

--- dek-d-frontend/src/App.vue
+++ dek-d-frontend/src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -39,10 +38,15 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
   </header>
 
-  <RouterView />
+  
 </template>
 
 <style scoped>
