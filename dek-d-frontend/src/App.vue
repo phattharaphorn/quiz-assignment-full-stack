390bc176a07a986119d10c3674a92552826091b4
phattharaphorn <64010659@kmitl.ac.th>
2024-01-23 00:28:35 +0700

    Update front-end

--- dek-d-frontend/src/App.vue
+++ dek-d-frontend/src/App.vue
@@ -43,6 +43,10 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
   </header>
 
